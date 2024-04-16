20ff2b8a95a11a16ce65d5c2397bdb6c3c6eab8e
Dorahely Sanchez <dorahelys@gmail.com>
2024-04-15 20:06:34 -0700

    updated code

--- javascript/recursive_count.js
+++ javascript/recursive_count.js
@@ -5,15 +5,22 @@ function recursiveCount(num = 0) {
   //now at 1 which is also less than 
 
   if (num < 10) {
+    //as long as number is less than 10 print
+    //start with 0 it is less than 10 so print 
     console.log("printing:", num)
+    //moves on to the next num after printing
+    //num = num +1
     num++
-    //console.log("num should be increasing:", num)
+    console.log("updated num:", num)
+    //calling recursive function again so since it started at 0 then moved to nect num 1
+    //now saying recursiveCount(1) which is less than 10 and process repeats
     recursiveCount(num)
-    return
-
-
+    //can also add the retun statement here to signal end of recursion
+    //return 
   }
-
+  //putting return outside the if statement because if inside the if statement any number is less than 10 
+  //so starting at 0 since 0 is less than then 10 the if statement will assume it is done and won't go until a number is greater 10
+  return 
 
   /*
    if (num >= 10) {
@@ -24,7 +31,6 @@ function recursiveCount(num = 0) {
    recursiveCount(num + 1);
    */
 }
-
 
 if (require.main === module) {
   recursiveCount();
